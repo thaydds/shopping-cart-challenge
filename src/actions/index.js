08410f1100f746dfc9127112c469196aea881c15
thaydds <thayronedds@gmail.com>
2019-10-23 00:01:41 -0300

    update action file with functions

--- src/actions/index.js
+++ src/actions/index.js
@@ -11,3 +11,35 @@ export const CART_CUPOM_ADD = 'CART_CUPOM_ADD'
 export const CART_CUPOM_SUB = 'CART_CUPOM_SUB'
 
 
+// actions functions
+
+export const cartProductAmoundAdd = (products, productName) => {
+    // make a clone 
+    const productClone = [...products.map( product => {
+        return Object.assign({}, product )
+    })]
+
+    //find product and add amount
+    const product = productClone.filter( product => product.name === productName)
+    product.amount += 1
+
+    return {
+        type: CART_PRODUCT_AMOUNT_ADD,
+        products: productClone
+    } 
+}
+
+export const cartProductAmoundSub = (products, productName) => {
+    
+    const productClone = [...products.map( product => {
+        return Object.assign({}, product )
+    })]
+    const product = productClone.filter( product => product.name === productName)
+    product.amount -= 1
+
+    return {
+        type: CART_PRODUCT_AMOUNT_ADD,
+        products: productClone
+    } 
+}
+
