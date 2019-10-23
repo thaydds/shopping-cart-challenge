f1aff0aaf8b897d2160f2a92d81b97715043dfc3
thaydds <thayronedds@gmail.com>
2019-10-22 23:54:50 -0300

    first test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import store from './store'
+import { Provider } from 'react-redux'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, div);
 });
