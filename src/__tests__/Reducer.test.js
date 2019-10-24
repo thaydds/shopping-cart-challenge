7c5cb76dc732f0637852fb22761de05d785d219d
thayrone.santos <thayrone.santos@dataprev.org.br>
2019-10-24 13:30:03 -0300

    update reducers test

--- src/__tests__/Reducer.test.js
+++ src/__tests__/Reducer.test.js
@@ -3,7 +3,7 @@ import * as Action from '../actions'
 
 describe('Cart reducer', () => {
     it('CART_PRODUCT_AMOUNT_ADD', () => {
-        const state = cart( initialState, Action.cartProductAmoundAdd('Banana'))
+        const state = cart( initialState, Action.cartProductAmoundAdd('Banana', initialState.products))
 
         const product = state.products.filter( p => p.name === 'Banana')
 
@@ -11,7 +11,8 @@ describe('Cart reducer', () => {
     })
 
     it('CART_PRODUCT_AMOUNT_SUB', () => {
-        const state = cart( initialState, Action.cartProductAmoundSub('Banana'))
+        const stateAdd = cart( initialState, Action.cartProductAmoundAdd('Banana', initialState.products))
+        const state = cart( {...stateAdd}, Action.cartProductAmoundSub('Banana', stateAdd.products))
 
         const product = state.products.filter( p => p.name === 'Banana')
 
@@ -19,49 +20,46 @@ describe('Cart reducer', () => {
     })
 
     it('CART_SUBTOTAL_CALC', () => {
-        let state = cart( initialState, Action.cartProductAmoundAdd('Banana'))
-        state = cart( state, Action.cartProductAmoundAdd('Apple'))
-        state = cart( state, Action.cartProductAmoundAdd('Banana'))
-
-        //subtotalcalc
-        state = cart( state, Action.cartSubtotalCalc( state.products, state.cupom))
+        let state = cart( initialState, Action.cartProductAmoundAdd('Banana', initialState.products))
+        state = cart( {...state}, Action.cartProductAmoundAdd('Banana', state.products))
+        state = cart( {...state}, Action.cartProductAmoundAdd('Apple', state.products))
+    
+        state = cart( {...state}, Action.cartSubtotalCalc( state.products, state.cupom))
 
         expect(state.subtotal).toStrictEqual(16)
         
     })
 
     it('CART_SHIPPING_CALC', () => {
-    
-        let state = cart( initialState, Action.cartShippingCalc( initialState.products, initialState.cupom, initialState.subtotal))
-    
+        let state = cart( initialState, Action.cartProductAmoundAdd('Banana', initialState.products))
+        state = cart( {...state}, Action.cartShippingCalc( state.products, state.cupom, state.subtotal))
         expect(state.shipping).toStrictEqual(30)
         
     })
 
     it('CART_TOTAL_CALC', () => {
     
-        let state = cart( initialState, Action.cartShippingCalc( initialState.products, initialState.cupom, initialState.subtotal))
-        state = cart( state, Action.cartSubtotalCalc( state.products, state.cupom))
-        state = cart( state, Action.cartTotalCalc( state.subtotal, state.shipping, state.cupom))
+        let state = cart( initialState, Action.cartProductAmoundAdd('Banana', initialState.products))
+        state = cart( {...state}, Action.cartProductAmoundAdd('Banana', state.products))
+        state = cart( {...state}, Action.cartProductAmoundAdd('Apple', state.products))
+        state = cart( {...state}, Action.cartSubtotalCalc( state.products, state.cupom))
+        state = cart( {...state}, Action.cartShippingCalc( state.products, state.cupom, state.subtotal))
+        state = cart( {...state}, Action.cartTotalCalc( state.subtotal, state.shipping, state.cupom))
 
         expect(state.total).toStrictEqual(46)
         
     })
 
-    it('CART_CUPOM_ADD', () => {
-    
+    it('CART_CUPOM_ADD', () => {    
         let state = cart( initialState, Action.cartCupomAdd( initialState.cupoms[0]))
-
-       
+    
         expect(state.cupom.key).toStrictEqual('A')
         
     })
 
     it('CART_CUPOM_SUB', () => {
-    
         let state = cart( initialState, Action.cartCupomSub( initialState.products, initialState.cupom))
 
-         expect(state.cupom.length).toStrictEqual(0)
-        
+        expect(state.cupom.length).toStrictEqual(0)
     })
 })
