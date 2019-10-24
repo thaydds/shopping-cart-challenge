28427ec262f4a046d692fd0d842d970ca58b19de
thaydds <thayronedds@gmail.com>
2019-10-24 04:24:47 -0300

    add snackbar message when cupom not found

--- src/components/Cupom.js
+++ src/components/Cupom.js
@@ -6,23 +6,36 @@ import Input from "@material-ui/core/Input"
 import Button from "@material-ui/core/Button"
 import {useDispatch, useSelector} from 'react-redux'
 import { cartCupomAdd} from '../actions'
+import SnackMessage from './SnackMessage'
 
 const Cupom = () => {
     const cart = useSelector(state => state)
     const dispatch = useDispatch()
     const [ cupomField, setCupomField] = useState('')
+    const [open, setOpen] = useState(false);
+
+    const handleClick = () => {
+        setOpen(true);
+    }
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+        return;
+        }
+        setOpen(false);
+    }
     
     const handleChange = () =>
         event => {
         setCupomField((event.target.value).toUpperCase().trim())
-    };
+    }
 
     const checkCupom = () => {
         const enabledCupom = cart.cupoms.filter( c => c.key === cupomField)
         if( enabledCupom.length > 0){
             dispatch(cartCupomAdd(enabledCupom))
         }else{
-            alert('cupom não encontrado')
+            handleClick()
         }
         setCupomField('') 
     }
@@ -42,6 +55,7 @@ const Cupom = () => {
             <Button data-testid='add-cupom' onClick={() => checkCupom()} variant="outlined" color="primary" >
                 Aplly
             </Button>
+            {open ? <SnackMessage handleClose={handleClose} open={open} /> : null}
             </ListItem>
       </List>
     )
---
+++ src/components/SnackMessage.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+
+const useStyles = makeStyles(theme => ({
+  close: {
+    padding: theme.spacing(0.5),
+  },
+
+}));
+
+const SnackMessage = (props) => {
+  const classes = useStyles();
+
+  const { handleClose, open } = props
+
+  console.log('oi')
+
+  return (
+    <div>
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        open={open}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        message={<span>[ERROR]:Coupon not found. Try: FOO, A, C</span>}
+        action={[
+          <IconButton
+            key="close"
+            aria-label="close"
+            color="inherit"
+            className={classes.close}
+            onClick={handleClose}
+          >
+            <CloseIcon />
+          </IconButton>,
+        ]}
+      />
+    </div>
+  );
+}
+
+export default SnackMessage
