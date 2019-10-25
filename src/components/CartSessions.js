0e60d335b9a867f859abf31e142d8d81969ac53c
thaydds <thayronedds@gmail.com>
2019-10-24 21:17:25 -0300

    fix total number of digits

--- src/components/CartSessions.js
+++ src/components/CartSessions.js
@@ -14,18 +14,18 @@ const CartSessions = (props) => {
         <List>
             <ListItem alignItems='center'>
                 <ListItemText primary={'Subtotal'} />
-                { cupom ? cupom.type ==='Percentual' ? <DiscountFlag label={`Percentual Cupom (-${cupom.effect * 100}%) of ${(100 * subtotal) / (100 - (cupom.effect * 100))}$`} /> : null : null }
-                <Typography data-testid='subtotal' variant="body2">{`${subtotal} $`}</Typography>
+                { cupom ? cupom.type ==='Percentual' ? <DiscountFlag label={`Percentual Cupom (-${cupom.effect * 100}%) of ${((100 * subtotal) / (100 - (cupom.effect * 100))).toFixed(0)}$`} /> : null : null }
+                <Typography data-testid='subtotal' variant="body2">{`${subtotal.toFixed(2)} $`}</Typography>
                 </ListItem>
                 <ListItem alignItems='center'>
                 <ListItemText primary={'Shipping'} />
                 { cupom ? cupom.type ==='Free Shipping' && subtotal >= cupom.min ? <DiscountFlag label={`Free Shipping Cupom`} /> : null : null }
-                <Typography data-testid='shipping' variant="body2">{ `${shipping} $` }</Typography>
+                <Typography data-testid='shipping' variant="body2">{ `${(shipping).toFixed(2)} $` }</Typography>
                 </ListItem>
                 <ListItem alignItems='center'>
                 <ListItemText primary={'Total'} />
                 { cupom ? cupom.type ==='Fixed' ? <DiscountFlag label={`Fixed Cupom -${cupom.effect}$`} /> : null : null }
-                <Typography data-testid='total' variant="subtitle2">{`${total} $`}</Typography>
+                <Typography data-testid='total' variant="subtitle2">{`${total.toFixed(2)} $`}</Typography>
             </ListItem>
         </List>
     )
