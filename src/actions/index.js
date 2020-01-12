c3ecd4cc80de65eb4722157363edb6640a77c6f4
thaydds <thayronedds@gmail.com>
2020-01-12 16:04:49 -0300

    create constants file

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,13 @@
-// actions constants
-export const CART_PRODUCT_AMOUNT_ADD = "PRODUCT_AMOUNT_ADD";
-export const CART_PRODUCT_AMOUNT_SUB = "PRODUCT_AMOUNT_SUB";
-
-export const CART_SUBTOTAL_CALC = "CART_SUBTOTAL_CALC";
-export const CART_TOTAL_CALC = "CART_TOTAL_CALC";
-export const CART_SHIPPING_CALC = "CART_SHIPPING_CALC";
-
-export const CART_COUPON_ADD = "CART_COUPON_ADD";
-export const CART_COUPON_SUB = "CART_COUPON_SUB";
-
-export const CART_RESET = "CART_RESET";
+import {
+  CART_PRODUCT_AMOUNT_ADD,
+  CART_PRODUCT_AMOUNT_SUB,
+  CART_SUBTOTAL_CALC,
+  CART_TOTAL_CALC,
+  CART_RESET,
+  CART_SHIPPING_CALC,
+  CART_COUPON_SUB,
+  CART_COUPON_ADD
+} from "../constants";
 
 export const cartProductAmoundAdd = (productName, products) => {
   let newProducts = products.map(product => {
--- src/store/index.js
+++ src/store/index.js
@@ -7,24 +7,10 @@ import {
   CART_RESET,
   CART_SHIPPING_CALC,
   CART_COUPON_SUB,
-  CART_COUPON_ADD
-} from "../actions";
-
-const products = [
-  { name: "Apple", desc: "sweet and delicious", price: 8, amount: 0 },
-  { name: "Banana", desc: "the best of the world", price: 4, amount: 0 },
-  { name: "Orange", desc: "orange orange", price: 3, amount: 0 }
-];
-
-// Percentual coupon: are coupons that reduce an amount in percentage of the cost on subtotal.
-// Fixed coupon: are coupons with fixed amounts that should reduce over the TOTAL.
-// Free Shipping: make the shipping price become 0 when applied, and should have a minimum subtotal requirement
-
-const enabledCoupons = [
-  { key: "A", type: "Percentual", effect: 0.3, active: false, min: 0 },
-  { key: "FOO", type: "Fixed", effect: 100, active: false, min: 0 },
-  { key: "C", type: "Free Shipping", effect: 0, active: false, min: 300.5 }
-];
+  CART_COUPON_ADD,
+  enabledCoupons,
+  products
+} from "../constants";
 
 export const initialState = {
   products: products,
